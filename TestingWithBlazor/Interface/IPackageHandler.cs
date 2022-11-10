d9110f87f5fa75138d417992f6cb6bb924953527
Ross Lewerenz <rgglewerenz@gmail.com>
2022-11-09 22:04:31 -0500

    Added functional minimizing to the app

--- TestingWithBlazor/Interface/IPackageHandler.cs
+++ TestingWithBlazor/Interface/IPackageHandler.cs
@@ -5,6 +5,7 @@ namespace TestingWithBlazor.Interface
 {
     public interface IPackageHandler
     {
+        public List<Package> Packages { get; set; }
         void AddPackage(Package new_package);
         List<Package> GetPackages();
         void SetPackages(List<Package> new_packages);
--- TestingWithBlazor/Service/PackageHandler.cs
+++ TestingWithBlazor/Service/PackageHandler.cs
@@ -5,9 +5,10 @@ using TestingWithBlazor.Interface;
 namespace TestingWithBlazor.Service
 {
     public class PackageHandler : IPackageHandler
-    {
-        private List<Package> packages { get; set; } = new List<Package>();
-        public List<Package> GetPackages() { return packages; }
+	{
+		private List<Package> packages { get; set; } = new List<Package>();
+        public List<Package> Packages { get { return packages; } set { packages = value; } }
+		public List<Package> GetPackages() { return packages; }
         public void SetPackages(List<Package> new_packages)
         {
             packages = new_packages;
