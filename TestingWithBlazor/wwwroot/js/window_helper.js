f5151a2eebdc2dacdd110cc812e3ab98e7893e52
Ross Lewerenz <rgglewerenz@gmail.com>
2022-11-14 18:04:27 -0500

    Added in the ability to create new browser windows, and the buttons work on the spesific window

--- TestingWithBlazor/wwwroot/js/window_helper.js
+++ TestingWithBlazor/wwwroot/js/window_helper.js
@@ -1,4 +1,10 @@
-﻿function isElectron() {
+﻿var GLOBAL = {};
+GLOBAL.DotNetReference = null;
+GLOBAL.SetDotnetReference = function (pDotNetReference) {
+    GLOBAL.DotNetReference = pDotNetReference;
+};
+
+function isElectron() {
     try {
         const { remote } = require('electron');
         return true;
@@ -16,3 +22,78 @@ window.addEventListener('load', (event) => {
        r.style.setProperty('--frame-size', '0px');
     }
 });
+
+function StartWatch(event) {
+    try {
+        const { remote } = require('electron');
+        const win = remote.getCurrentWindow();
+
+        win.on('maximize', onWindowMove);
+        win.on('unmaximize', onWindowMove);
+        win.on('move', onWindowMove);
+        
+    } catch {
+        return 0;
+    }
+}
+
+
+function onWindowMove() {
+    GLOBAL.DotNetReference.invokeMethodAsync('OnWindowChange');
+}
+
+function closeCurrentWindow() {
+    try {
+        const { remote } = require('electron');
+        var window = remote.getCurrentWindow();
+        window.close();
+    }
+    catch {
+        return;
+    }
+    
+}
+
+function getIsFullScreen() {
+    try {
+        const { remote } = require('electron');
+        const window = remote.getCurrentWindow();
+        return window.isMaximized();
+    }
+    catch {
+        return false;
+    }
+}
+
+function minimizeCurrentWindow() {
+    try {
+        const { remote } = require('electron');
+        const window = remote.getCurrentWindow();
+        window.minimize();
+    }
+    catch  {
+        return;
+    }
+}
+
+function maximizeCurrentWindow() {
+    try {
+        const { remote } = require('electron');
+        const window = remote.getCurrentWindow();
+        window.maximize();
+    }
+    catch {
+        return;
+    }
+}
+
+function restoreCurrentWindow() {
+    try {
+        const { remote } = require('electron');
+        const window = remote.getCurrentWindow();
+        window.restore();
+    }
+    catch {
+        return;
+    }
+}
