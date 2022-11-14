96693b26de9803d8d8a72f894453ffc401d69e3d
Ross Lewerenz <rgglewerenz@gmail.com>
2022-11-14 11:22:26 -0500

    Added the view all packages page, and changed the layout of the edit/add package forms, and changed the detailed package view for the view all packages page

---
+++ TestingWithBlazor/wwwroot/js/row_helper.js
@@ -0,0 +1,28 @@
+﻿
+function row_load(class_name) {
+    var elements = document.getElementsByClassName(class_name);
+    for (var i = 0; i < elements.length; i++) {
+        do_onto(elements[i]);
+    }
+}
+
+function do_onto(element) {
+    var count = element.childElementCount;
+    const width = 1 / count;
+    console.log(count);
+    console.log(element);
+    console.log(width);
+    var children = element.children;
+    for (var i = 0; i < children.length; i++) {
+        change_width(children[i], width);
+    }
+}
+
+function change_width(element, percentage) {
+    console.log(element);
+    percentage = percentage * 100;
+    console.log(percentage);
+    element.style.width = percentage + '%';
+    console.log('here');
+
+}
